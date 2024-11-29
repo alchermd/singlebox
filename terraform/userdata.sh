76dfba69de09af9a4ee9be0f2923c1cd974c5d72
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2024-11-29 18:45:07 +0800

    Install docker and compose on load

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,90 +1,38 @@
 #!/bin/bash -xe
 
-## CodeDeploy Agent Bootstrap Script##
+## CodeDeploy Agent Bootstrap Script for Amazon Linux ##
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-AUTOUPDATE=false
 
 function installdep(){
-
-if [ ${PLAT} = "ubuntu" ]; then
-
-  apt-get -y update
-  # Satisfying even ubuntu older versions.
-  apt-get -y install jq awscli ruby2.0 || apt-get -y install jq awscli ruby
-
-elif [ ${PLAT} = "amz" ]; then
   yum -y update
   yum install -y aws-cli ruby jq
-
-fi
-}
-function platformize(){
-
-#Linux OS detection#
- if hash lsb_release; then
-   echo "Ubuntu server OS detected"
-   export PLAT="ubuntu"
-
-elif hash yum; then
-  echo "Amazon Linux detected"
-  export PLAT="amz"
-
- else
-   echo "Unsupported release"
-   exit 1
- fi
 }
 
 function execute(){
-if [ ${PLAT} = "ubuntu" ]; then
-
   cd /tmp/
   wget https://aws-codedeploy-${REGION}.s3.amazonaws.com/latest/install
   chmod +x ./install
 
-  if ./install auto; then
-    echo "Installation completed"
-      if ! ${AUTOUPDATE}; then
-            echo "Disabling Auto Update"
-            sed -i '/@reboot/d' /etc/cron.d/codedeploy-agent-update
-            chattr +i /etc/cron.d/codedeploy-agent-update
-            rm -f /tmp/install
-      fi
-    exit 0
-  else
+  if ! ./install auto; then
     echo "Installation script failed, please investigate"
     rm -f /tmp/install
     exit 1
   fi
 
-elif [ ${PLAT} = "amz" ]; then
-  cd /tmp/
-  wget https://aws-codedeploy-${REGION}.s3.amazonaws.com/latest/install
-  chmod +x ./install
-
-    if ./install auto; then
-      echo "Installation completed"
-        if ! ${AUTOUPDATE}; then
-            echo "Disabling auto update"
-            sed -i '/@reboot/d' /etc/cron.d/codedeploy-agent-update
-            chattr +i /etc/cron.d/codedeploy-agent-update
-            rm -f /tmp/install
-        fi
-      exit 0
-    else
-      echo "Installation script failed, please investigate"
-      rm -f /tmp/install
-      exit 1
-    fi
-
-else
-  echo "Unsupported platform ''${PLAT}''"
-fi
-
+  echo "Installing Docker"
+  yum -y install docker
+  systemctl enable docker
+  systemctl start docker
+  usermod -a -G docker ec2-user
+  curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  docker version
+  docker-compose version
+  echo "Installation completed"
+  exit 0
 }
 
-platformize
 installdep
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN"  -v http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ".region")
